9804047c67b3488b71221075683225e409a357f2
Joach27 <reekgold26@gmail.com>
2025-10-16 11:05:34 +0100

    feat: replace App.css with clair.css for improved styling

--- front/chatbot/src/App.jsx
+++ front/chatbot/src/App.jsx
@@ -3,7 +3,8 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import "./App.css";
+// import "./App.css";
+import "./style/clair.css"
 
 import { ErrorBoundary } from "./ErrorBoundary";
 
